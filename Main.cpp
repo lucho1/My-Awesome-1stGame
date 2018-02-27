9b05f574586e5a667973587cf0ee088e958faa3d
lucho1 <luchosuaya@hotmail.com>
2018-02-27 10:30:03 +0100

    Upload 3

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,11 @@
+#include "SDL/include/SDL.h"   // The"\" slash is only used in windows and is better to use the "/" slash
+#pragma comment(lib, "SDL/libx86/SDL2.lib") // This access the preprocessor directive specifically to the Visual S compiler
+#pragma comment(lib, "SDL/libx86/SDL2main.lib") //So, finally is to access to the library
 
 
-int main() {
+int main(int argc, char* argv[]) {
 	
+
 
 	return 0;
 }
