c580780df0b119b1de749e88eca605cee28def24
lucho1 <luchosuaya@hotmail.com>
2018-03-05 18:56:48 +0100

    Update movement and shooting

--- Main.cpp
+++ Main.cpp
@@ -52,6 +52,10 @@ int main(int argc, char* argv[]) {
 	rect.h = 100;
 	rect.w = 120;
 
+	SDL_Rect bullet;
+	bullet.h = 5;
+	bullet.w = 50;
+
 	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); //Here we set now the color that the renderer will use to draw (red is rgb 255 0 0)
 	SDL_RenderFillRect(renderer, &rect); //Put the rectangle on the screen we can put "Draw" instead of "Fill", but it will only draw the edges
 	SDL_RenderPresent(renderer); //Render the changes above
@@ -68,38 +72,62 @@ int main(int argc, char* argv[]) {
 
 	// Let's create a event "e" and a bool to know when we're pressing a key
 	
-	SDL_Event e;  
 	bool quit = false;
 
 	while (!quit){
 
+		SDL_Event e;
 		while (SDL_PollEvent(&e)) {
 
-			if (e.type == SDL_QUIT) { //SDL_QUIT is to the X at the top/right of the screen. Also, SDL_KEYDOWN is to detect a key pressed
+			if (e.type == SDL_KEYDOWN) {
+
+				switch (e.key.keysym.scancode) {
+
+				case SDL_SCANCODE_LEFT:
+					rect.x -= 5;
+					break;
+				case SDL_SCANCODE_RIGHT:
+					rect.x += 5;
+					break;
+				case SDL_SCANCODE_UP:
+					rect.y -= 5;
+					break;
+				case SDL_SCANCODE_DOWN:
+					rect.y += 5;
+					break;
+
+				case SDL_SCANCODE_SPACE:
+					bullet.x = rect.x + 10;
+					bullet.y = rect.y - 10;
+					while (bullet.x < 600) { 
+						
+						bullet.x++;
+						SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+						SDL_RenderFillRect(renderer, &bullet);
+						SDL_RenderPresent(renderer);
+					
+					}
+				}
+			}
+
+			else if (e.type == SDL_QUIT ||e.type == SDL_SCANCODE_ESCAPE) { //SDL_QUIT is to the X at the top/right of the screen. Also, SDL_KEYDOWN is to detect a key pressed
 
 				quit = true;
 			}
 		}
 
-		while (rect.x < 550 || rect.y < 350){
-
-				rect.x++;
-				rect.y++;
-
-				SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
-				SDL_RenderClear(renderer);
-				SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-				SDL_RenderFillRect(renderer, &rect);
-				SDL_RenderPresent(renderer);
-				SDL_Delay(10);
-			
-		}
-
-		rect.x = -120;
-		rect.y = -100;
+		SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+		SDL_RenderClear(renderer);
+		SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+		SDL_RenderFillRect(renderer, &rect);
+		SDL_RenderPresent(renderer);
+		SDL_Delay(10);
 
 	}
 	
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 	return 0;
 }
 
